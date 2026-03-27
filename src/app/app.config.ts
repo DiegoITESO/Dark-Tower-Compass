5029402415bcf483012d218e404bf5943e0ebe15
ELI-626 <eli80839@gmail.com>
2026-03-26 20:45:15 -0600

    chore: update AngularFire and Firebase dependencies; configure Firebase in app

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,35 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+
 
 import { routes } from './app.routes';
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyA8FHpMHw0HWw5Gt4QmnVX1nWUaWtN0XNE",
+
+  authDomain: "dark-tower-compass.firebaseapp.com",
+
+  projectId: "dark-tower-compass",
+
+  storageBucket: "dark-tower-compass.firebasestorage.app",
+
+  messagingSenderId: "527864079727",
+
+  appId: "1:527864079727:web:afa6bb2a391e5af086349a"
+
+};
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth()),
   ]
 };
