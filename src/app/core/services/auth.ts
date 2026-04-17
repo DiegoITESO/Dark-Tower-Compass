b1df0db09e09d9163dbc9095a6c0f338d4775840
DiegoITESO <diego.gomezm@iteso.mx>
2026-04-17 14:57:03 -0600

    feat: implement authentication service with registration and login functionality; enhance auth component with form handling and animations

--- src/app/core/services/auth.ts
+++ src/app/core/services/auth.ts
@@ -1,6 +1,48 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject, signal } from '@angular/core';
+import { 
+  Auth as FirebaseAuth, 
+  user, 
+  User,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+ } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Auth {}
+export class Auth {
+  private auth = inject(FirebaseAuth);
+  private router = inject(Router);
+
+  userSignal = signal<User | null | undefined>(undefined);
+
+  constructor() {
+    user(this.auth).subscribe((user) => {
+      this.userSignal.set(user);
+    });
+  }
+
+  async register(email: string, pass: string) {
+    try {
+      const credential = await createUserWithEmailAndPassword(this.auth, email, pass);
+      return credential;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async login(email: string, pass: string) {
+    try {
+      return await signInWithEmailAndPassword(this.auth, email, pass);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async logout() {
+    await signOut(this.auth);
+    this.router.navigate(['/login']);
+  }
+}
--- src/app/features/auth/auth.ts
+++ src/app/features/auth/auth.ts
@@ -1,9 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Auth as AuthService } from '../../core/services/auth';
+import { fadeInSlideUp } from '../../shared/animations/fade-in.animations'
 
 @Component({
   selector: 'app-auth',
-  imports: [],
+  standalone: true,
+  imports: [ FormsModule ],
+  animations: [ fadeInSlideUp ],
   templateUrl: './auth.html',
   styleUrl: './auth.css',
 })
-export class Auth {}
+export class Auth {
+  authService = inject(AuthService);
+  isLogin = signal(true);
+  loading = signal(false);
+  email = '';
+  password = '';
+  
+  toggleMode() {
+    this.isLogin.update(v => !v);
+  }
+
+  async handleSubmit() {
+    if (!this.email || !this.password) return;
+    
+    this.loading.set(true);
+    try {
+      if (this.isLogin()) {
+        await this.authService.login(this.email, this.password);
+      } else {
+        await this.authService.register(this.email, this.password);
+      }
+      // Navegación exitosa manejada por el servicio o aquí
+    } catch (error) {
+      console.error("Fallo en la conexión con la Torre:", error);
+    } finally {
+      this.loading.set(false);
+    }
+  }
+}
