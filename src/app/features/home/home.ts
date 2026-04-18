5fe55e7a515cdaf553acdec6aba55819a8ba5e6b
DiegoITESO <diego.gomezm@iteso.mx>
2026-04-17 18:05:58 -0600

    feat: enhance authentication form with validation and error handling; improve user experience with feedback messages

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,10 +1,13 @@
+//------ Imports ------//
 import { Component } from '@angular/core';
 import { CharacterCard } from '../../shared/components/character-card/character-card';
 
+//------ Injectable decorator ------//
 @Component({
   selector: 'app-home',
   imports: [CharacterCard],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
+//------ Service Auth ------//
 export class Home {}
