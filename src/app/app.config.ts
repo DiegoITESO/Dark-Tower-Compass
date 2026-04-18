2053f56a337faec8a3d8def04c114f59af86cf8d
Fabricio1210 <fabriciodlvalencia@gmail.com>
2026-04-17 19:47:45 -0600

    fix: character cards

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,19 +6,22 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { getStorage, provideStorage } from '@angular/fire/storage';
 
 import { routes } from './app.routes';
 
 //------ Configuration for Firebase ------//
 const firebaseConfig = {
-  apiKey: "AIzaSyB9s-WiC_8-6MwS0a3T_CqS_3fKeHAAR1c",
-  authDomain: "prueba-dark-compass.firebaseapp.com",
-  projectId: "prueba-dark-compass",
-  storageBucket: "prueba-dark-compass.firebasestorage.app",
-  messagingSenderId: "922892322920",
-  appId: "1:922892322920:web:72cfca9cb1f79491cfd3dd",
-  measurementId: "G-58P3QBP1JV"
+  apiKey: 'AIzaSyA8FHpMHw0HWw5Gt4QmnVX1nWUaWtN0XNE',
+
+  authDomain: 'dark-tower-compass.firebaseapp.com',
+
+  projectId: 'dark-tower-compass',
+
+  storageBucket: 'dark-tower-compass.firebasestorage.app',
+
+  messagingSenderId: '527864079727',
+
+  appId: '1:527864079727:web:afa6bb2a391e5af086349a',
 };
 
 //------ Configuration for App ------//
@@ -29,7 +32,6 @@ export const appConfig: ApplicationConfig = {
     provideFirebaseApp(() => initializeApp(firebaseConfig)),
     provideFirestore(() => getFirestore()),
     provideAuth(() => getAuth()),
-    provideStorage(() => getStorage()),
     provideAnimationsAsync(),
   ],
 };
